7319ed51057d778add19cbc0379a83a52487c980
Mohammed ADDI <mohammed.addi@epita.fr>
2025-06-22 23:19:49 +0200

    test

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -159,7 +159,7 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
     <div className="relative flex items-center gap-2 text-lg sm:text-xl md:text-2xl">
       <Globe className="w-4 h-4 sm:w-5 sm:h-5 text-indigo-600" />
       <span className="bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
-        <TypewriterEffect text="https://ai-mohammed.github.io/my-portfolio" />
+        <TypewriterEffect text="https://mohammed-addi.netlify.app" />
       </span>
     </div>
   </div>
