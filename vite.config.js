b7f01c0d685c2fe6cb4203a5c533d8a6b17c6553
Mohammed ADDI <mohammed.addi@epita.fr>
2025-06-22 20:31:08 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/my-portfolio/",  
+  base: process.env.NODE_ENV === "production" ? "/my-portfolio/" : "/",
   plugins: [react()],
 })
